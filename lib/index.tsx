e56bf487a6ae7352d81d52f4eca59577828abc08
Mr.X <397906946@qq.com>
2019-06-06 15:44:07 +0800

    发布代码

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,15 +1,2 @@
-import Icon from './icon/icon';
-import * as ReactDOM from 'react-dom';
-import * as React from 'react';
+export {default as Icon} from './icon/icon';
 
-const fn: React.MouseEventHandler = (e) => {
-  console.log(e.target);
-};
-
-ReactDOM.render(<div>
-  <Icon name="wechat"
-        onClick={fn}
-        onMouseEnter={() => console.log('enter')}
-        onMouseLeave={() => console.log('leave')}
-  />
-</div>, document.getElementById('root'));
